e72825a1d4b7f68afb9e0fd8f8275e5a3ed2d5c8
RICHARD BARBOSA PINHEIRO <39630333X@alunos.etec.itaqua>
2019-04-11 22:28:28 -0300

    Criado pagina de adicionar nova cerveja e excluido paginas antigas

---
+++ src/app/add-cerveja/add-cerveja.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-cerveja',
+  templateUrl: './add-cerveja.page.html',
+  styleUrls: ['./add-cerveja.page.scss'],
+})
+export class AddCervejaPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    this.mensagem = ""
+  }
+
+  mensagem;
+  
+salvar(form){
+  const cervejaString = JSON.stringify(form.value)
+  const nomeCerveja = form.value.nome
+  
+  sessionStorage.setItem(nomeCerveja,cervejaString)
+
+  form.reset()
+  this.mensagem = 'cadastrado com sucesso!';
+  }  
+
+}
